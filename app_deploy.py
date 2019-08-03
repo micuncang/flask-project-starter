60018a128efb6508b303e1f4ab6d902c42c4f1b4
micuncang <micuncang@gmail.com>
2019-08-03 16:00:10 +0800

    code refactor

--- app_deploy.py
+++ app_deploy.py
@@ -1,3 +1,3 @@
-from model_app import create_app 
+from model_app import create_app
 
 app = create_app('config.py')
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn -b 0.0.0.0:5000 -p pid -w 2 -e APP_CONFIG_FILE=`pwd`/config/env/development.py app_deploy:app -D
+gunicorn -b 0.0.0.0:5000 -p pid -w 2 -e APP_CONFIG_FILE="$(pwd)"/config/env/development.py app_deploy:app -D
